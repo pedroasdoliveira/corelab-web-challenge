d1db602694b34086d9cb1e7f0144314d088e8a3e
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-11 16:24:29 -0300

    feat(Card): implement modifications on component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,48 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+import DeleteIcon from "../../assets/icon/delete.png";
+import EditIcon from "../../assets/icon/edit.png";
+import FavoriteIcon from "../../assets/icon/favorite.png";
 import styles from "./Card.module.scss";
 
 interface ICard {
   title: string;
+  id: number | undefined;
   children: ReactNode;
 }
 
 const Card = (props: ICard) => {
+  const navigate = useNavigate();
+
+  const handleEdit = () => {
+    navigate(`/:${props.id}`);
+  };
+
+  const handleDelete = async () => {};
+
   return (
     <>
       <div className={styles.Card}>
+        <div className={styles.IconsContainer}>
+          <img
+            className={styles.EditCard}
+            src={EditIcon}
+            alt="Icone de editar o card"
+            onClick={handleEdit}
+          />
+          <img
+            className={styles.DeleteCard}
+            src={DeleteIcon}
+            alt="Icone de deletar o card"
+            onClick={handleDelete}
+          />
+          <img
+            className={styles.FavoriteIcon}
+            src={FavoriteIcon}
+            alt="Icone de favoritar o card"
+          />
+        </div>
         <h2>{props.title}</h2>
-
         <div className={styles.content}>{props.children}</div>
       </div>
     </>
